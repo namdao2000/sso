ec57cafd17094c4ff472c1c3491ff1f0384ee2cc
Nam Dao <nam.dao@eucalyptus.vc>
2022-01-28 23:18:28 +1000

    fix: comment in generate script

--- generate-ecdsa-keypair.sh
+++ generate-ecdsa-keypair.sh
@@ -4,7 +4,7 @@ openssl ecparam -name prime256v1 -genkey -noout -out private.ec.key
 # Convert it to pkcs8 format
 openssl pkcs8 -topk8 -nocrypt -in private.ec.key -out private.pem
 
-# Get public key (But its not in pkcs8 format wtf)
+# Get public key
 openssl ec -in private.pem -pubout -out public.pem
 
 echo "PRIVATE_KEY="$(cat private.pem)"" > .env
