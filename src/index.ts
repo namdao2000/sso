4f1b8c98469da148bae8c915b5529456b59a35ed
Nam Dao <nam.dao@eucalyptus.vc>
2022-01-28 18:57:56 +1000

    tried allowing origin to anywhere

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,12 @@ import { PUBLIC_KEY_EXPORT } from './utils/constants';
 import { LoginController } from './controllers/login.controller';
 import { SignupController } from './controllers/signup.controller';
 import { VerifyJWTController } from './controllers/VerifyJWT.controller';
+import * as CORS from 'worktop/cors';
 
 const router = new Router();
+router.prepare = CORS.preflight({
+  origin: '*',
+});
 
 router.add('GET', '/', async (req: ServerRequest, res: ServerResponse) => {
   res.send(200, { message: 'This is an sso server. Documentations are WIP' });
